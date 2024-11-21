c99bbcf8655fc092869d40a4ea57abc060e0a3fa
bhaveshdandavate <bhaveshdandavate2@gmail.com>
2024-11-21 15:07:46 +0530

    javascript practice

---
+++ functions/script.js
@@ -0,0 +1,45 @@
+//function
+// function keyword(msg,n){
+//     console.log(msg,n);
+// }
+
+// keyword("im omkar",100);
+
+//function for sum of to numbers
+// function sum(a,b){
+// return a+b;
+// }
+
+//arrow function
+const mul = (a, b) => {
+  return a * b;
+};
+
+//Create a function using the function keyword that takes a string as an argument and returns the number of vowels in the string.
+// function countvowel(str){
+// let count=0;
+// for(const char of str){
+//     if (char==="a"||char==="e"||char==="i"||char==="o"||char==="u"){
+//         count++;
+//     }
+// }
+// console.log(count);
+// }
+
+//create an arrow function to perform same task
+// const countvowel = (str) => {
+//   let count = 0;
+//   for (const char of str) {
+//     if (
+//       char === "a" ||
+//       char === "e" ||
+//       char === "i" ||
+//       char === "o" ||
+//       char === "u"
+//     ) {
+//       count++;
+//     }
+//   }
+//   console.log(count);
+// };
+
---
+++ loops/script.js
@@ -0,0 +1,79 @@
+// for loop
+// for(let i=1; i<=10;i++){
+//     console.log("Mbet India");
+// }
+
+// for(let i=1; i<=10;i++){
+//     console.log("i=",i);
+// }
+
+
+// calculate sum of 100
+// sum=0;
+// n=100;
+// for(let i=1; i<=n; i++){
+//     sum=sum+i;
+// }
+// console.log("sum=",sum);
+
+
+// while loop
+// let i=1;
+// while(i<=10){
+//     console.log("Mbet India");
+//     i++;
+// }
+
+
+//do while loop
+// let i=1;
+// do{
+//     console.log("Mbet India");
+//     i++;
+// }while(i<=10);
+
+
+
+ // for of loop
+// let str="MBETINDIA";
+// for(let i of str){
+//     console.log("i",i);
+//}
+
+// let str="omarpradipyadav";
+// let size=0;
+// for(let i of str){
+//     console.log("i",i)
+//     size++;
+// }
+// console.log("string size",size);
+
+
+// for in loop
+// let student={
+//     name:"omkar yadav",
+//     age:19,
+//     rollno:15,
+//     marks:75.40,
+// }
+// for(let i in student){
+//     console.log("i=",i ,"value=",student[i]);
+// }
+
+//print all even numbers 0 to 100
+// n=100;
+// i=1;
+// for(let i=0; i<=n; i++){
+//     if(i%2 == 0){  //print even number using if(i%2 !== 0);
+//         console.log("num=",i);
+//     }
+// }
+
+//create a game where you start with any random gane number ask the user to keep guessing the game number until the user enter correct value.
+// let gameNum=25;
+// let userNum=prompt("Guess the game number:");
+
+// while(userNum != gameNum){
+//    userNum= prompt(" You entered wrong answer, Guess again:");
+// }
+// console.log("congratulation,You entered the right answer");
